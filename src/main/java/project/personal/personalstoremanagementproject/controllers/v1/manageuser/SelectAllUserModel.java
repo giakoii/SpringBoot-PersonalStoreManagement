28efcf259220672f4f6d370b8a24f19335d18660
giakoii <khoipgse172073@fpt.edu.vn>
2024-12-29 23:33:20 +0700

    [Add][SelectAllUser] Init API SelectAllUser

---
+++ src/main/java/project/personal/personalstoremanagementproject/controllers/v1/manageuser/SelectAllUserModel.java
@@ -0,0 +1,27 @@
+package project.personal.personalstoremanagementproject.controllers.v1.manageuser;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+
+@Getter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
+public class SelectAllUserModel {
+    String username;
+
+    String fullName;
+
+    LocalDate dateOfBirth;
+
+    String gender;
+
+    String email;
+
+    String city;
+
+    String country;
+}
---
+++ src/main/java/project/personal/personalstoremanagementproject/controllers/v1/manageuser/SelectAllUserResponse.java
@@ -0,0 +1,8 @@
+package project.personal.personalstoremanagementproject.controllers.v1.manageuser;
+
+import project.personal.personalstoremanagementproject.controllers.AbstractApiResponse;
+
+import java.util.List;
+
+public class SelectAllUserResponse extends AbstractApiResponse<List<SelectAllUserModel>> {
+}
