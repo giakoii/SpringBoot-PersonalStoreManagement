aa1cb15e678d137b23b102858b7746d1c39570fd
giakoii <khoipgse172073@fpt.edu.vn>
2024-12-29 23:31:58 +0700

    [Mod][UpdateUserProfile] Mod UpdateUserProfile

--- src/main/java/project/personal/personalstoremanagementproject/controllers/v1/user/profilescreen/controllers/UpdateUserProfileController.java
+++ src/main/java/project/personal/personalstoremanagementproject/controllers/v1/user/profilescreen/controllers/UpdateUserProfileController.java
@@ -1,12 +1,14 @@
 package project.personal.personalstoremanagementproject.controllers.v1.user.profilescreen.controllers;
 
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import project.personal.personalstoremanagementproject.controllers.AbstractApiController;
 import project.personal.personalstoremanagementproject.controllers.v1.user.profilescreen.request.UpdateUserProfileRequest;
 import project.personal.personalstoremanagementproject.controllers.v1.user.profilescreen.responses.UpdateUserProfileResponse;
+import project.personal.personalstoremanagementproject.entities.Address;
+import project.personal.personalstoremanagementproject.entities.UserAccount;
 import project.personal.personalstoremanagementproject.exceptions.DetailError;
+import project.personal.personalstoremanagementproject.repositories.AddressRepository;
 import project.personal.personalstoremanagementproject.repositories.UserRepository;
 import project.personal.personalstoremanagementproject.services.JwtService;
 import project.personal.personalstoremanagementproject.utils.MessageId;
@@ -17,17 +19,20 @@ import java.util.List;
  * Controller for updating user profile
  */
 @RestController
-@RequestMapping("/api/v1/user/update-profile")
+@RequestMapping("/api/v1/UpdateUserProfile")
 public class UpdateUserProfileController extends AbstractApiController<UpdateUserProfileRequest, UpdateUserProfileResponse, String> {
 
+    private final AddressRepository addressRepository;
+    
     /**
      * Constructor
-     * @param authenticationManager
      * @param userRepository
      * @param jwtService
+     * @param addressRepository
      */
-    public UpdateUserProfileController(AuthenticationManager authenticationManager, UserRepository userRepository, JwtService jwtService) {
+    public UpdateUserProfileController(UserRepository userRepository, JwtService jwtService, AddressRepository addressRepository) {
         super(userRepository, jwtService);
+        this.addressRepository = addressRepository;
     }
 
     /**
@@ -40,29 +45,25 @@ public class UpdateUserProfileController extends AbstractApiController<UpdateUse
         var response = new UpdateUserProfileResponse();
 
         var user = getCurrentUser();
-        if (user == null) {
-            response.setSuccess(false);
-            response.setMessage(MessageId.E0000, "User not authenticated");
-            return response;
-        }
         // Update user information
-        if (request.getFullName() != null){
-            user.setFullName(request.getFullName());
-        }
-        if (request.getNickName() != null){
-            user.setNickName(request.getNickName());
-        }
-        if (request.getPhoneNumber() != null){
-            user.setPhoneNumber(request.getPhoneNumber());
-        }
-        if (request.getAddress() != null){
-            user.setAddress(request.getAddress());
-        }
-        if (request.getAvatarUrl() != null){
-            user.setAvatarUrl(request.getAvatarUrl());
-        }
+        updateUserInformation(request, user);
         saveChange(user, request, false);
         userRepository.save(user);
+
+        // Update address information
+        var addressUser = addressRepository.findByUserId(user.getUserId());
+        if (addressUser.isEmpty()) {
+            // Create new address
+            Address address = new Address();
+            updateAddressUser(request, address, user.getUserId());
+            saveChange(address, request, true);
+            addressRepository.save(address);
+        } else {
+            // Update address
+            updateAddressUser(request, addressUser.get(), user.getUserId());
+            saveChange(addressUser.get(), request, true);
+            addressRepository.save(addressUser.get());
+        }
 
         // True
         response.setSuccess(true);
@@ -85,5 +86,56 @@ public class UpdateUserProfileController extends AbstractApiController<UpdateUse
             return response;
         }
         return null;
+    }
+
+    /**
+     * Update user information
+     * @param request the request to update
+     * @param user the user to update
+     */
+    private void updateUserInformation(UpdateUserProfileRequest request, UserAccount user) {
+            if (request.getFullName() != null) {
+                user.setFullName(request.getFullName());
+            }
+            if (request.getNickName() != null) {
+                user.setNickName(request.getNickName());
+            }
+            if (request.getPhoneNumber() != null) {
+                user.setPhoneNumber(request.getPhoneNumber());
+            }
+            if (request.getAvatarUrl() != null) {
+                user.setAvatarUrl(request.getAvatarUrl());
+            }
+            if (request.getGender() != null) {
+                user.setGender(request.getGender());
+            }
+            if (request.getDateOfBirth() != null) {
+                user.setDateOfBirth(request.getDateOfBirth());
+            }
+    }
+
+    /**
+     * Update address information
+     * @param request the request to update
+     * @param address the address to update
+     * @param userId the user id
+     */
+    private void updateAddressUser(UpdateUserProfileRequest request, Address address, Long userId) {
+        if (request.getAddressLine() != null) {
+            address.setAddressLine(request.getAddressLine());
+        }
+        if (request.getCity() != null) {
+            address.setCity(request.getCity());
+        }
+        if (request.getState() != null) {
+            address.setState(request.getState());
+        }
+        if (request.getCountry() != null) {
+            address.setCountry(request.getCountry());
+        }
+        if (request.getZipCode() != null) {
+            address.setZipCode(request.getZipCode());
+        }
+        address.setUserId(userId);
     }
 }
