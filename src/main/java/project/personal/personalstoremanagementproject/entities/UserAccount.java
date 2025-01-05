6f3f8597e07fb3a2c02d8b7f6f06616910abf5c9
giakoii <khoipgse172073@fpt.edu.vn>
2025-01-05 17:39:39 +0700

    [Mod][UserAccount] Mod var in UserAccount class

--- src/main/java/project/personal/personalstoremanagementproject/entities/UserAccount.java
+++ src/main/java/project/personal/personalstoremanagementproject/entities/UserAccount.java
@@ -1,12 +1,17 @@
 package project.personal.personalstoremanagementproject.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
+import lombok.experimental.FieldDefaults;
 import org.hibernate.annotations.Nationalized;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import project.personal.personalstoremanagementproject.utils.constants.ConstantEnum;
+
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Collection;
@@ -16,58 +21,49 @@ import java.util.List;
 @Setter
 @Entity
 @Builder
-@Table(name = "UserAccount")
-@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 @AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "user_account")
 public class UserAccount extends BaseEntity implements UserDetails {
     @Id
-    @Column(name = "UserId", nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", nullable = false)
     private Long userId;
 
     @Nationalized
-    @Column(name = "Username", nullable = false, length = 50)
+    @Lob
+    @Column(name = "avatar_url")
+    private String avatarUrl;
+
+    @Nationalized
+    @Column(name = "email", nullable = false, length = 255)
+    private String email;
+
+    @Column(name = "gender")
+    @Enumerated(EnumType.STRING)
+    private ConstantEnum.Gender gender;
+
+    @Column(name = "last_login")
+    private Instant lastLogin;
+
+    @Nationalized
+    @Column(name = "username", nullable = false, length = 50)
     private String username;
 
     @Nationalized
-    @Column(name = "Email", nullable = false, length = 255)
-    private String email;
-
-    @Nationalized
     @Lob
-    @Column(name = "PasswordHash", nullable = false)
+    @Column(name = "password_hash", nullable = false)
     private String passwordHash;
 
+    @Size(max = 15)
     @Nationalized
-    @Column(name = "FullName", nullable = true, length = 255)
-    private String fullName;
-
-    @Nationalized
-    @Column(name = "NickName", nullable = true, length = 255)
-    private String nickName;
-
-    @Nationalized
-    @Column(name = "PhoneNumber", nullable = true, length = 15)
+    @Column(name = "phone_number", length = 15)
     private String phoneNumber;
-
-    @Nationalized
-    @Lob
-    @Column(name = "AvatarUrl")
-    private String avatarUrl;
-
-    @Column(name = "DateOfBirth")
-    private LocalDate dateOfBirth;
-
-    @Column(name = "Gender", length = 50)
-    @Enumerated(EnumType.STRING)
-    private ConstantEnum.Gender gender;
 
     @Column(name = "Role", length = 50)
     @Enumerated(EnumType.STRING)
     private ConstantEnum.Role role;
-
-    @Column(name = "LastLogin")
-    LocalDateTime lastLogin;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
@@ -98,4 +94,5 @@ public class UserAccount extends BaseEntity implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
+
 }
