de59116fceec49335b9a418f1ce3b739be436c2b
giakoii <khoipgse172073@fpt.edu.vn>
2025-01-15 21:19:59 +0700

    [Update][OrderRepository] Insert @Procedure(name = "CreateOrder")

--- src/main/java/project/personal/personalstoremanagementproject/repositories/OrderRepository.java
+++ src/main/java/project/personal/personalstoremanagementproject/repositories/OrderRepository.java
@@ -1,6 +1,14 @@
 package project.personal.personalstoremanagementproject.repositories;
 
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
 import project.personal.personalstoremanagementproject.entities.Order;
 
 public interface OrderRepository extends BaseRepository<Order, Long> {
+    @Procedure(name = "CreateOrder")
+    Object[] createOrder(
+            @Param("CustomerId") Long customerId,
+            @Param("CreatedBy") Long createBy,
+            @Param("OrderDetails") String orderDetails
+    );
 }
